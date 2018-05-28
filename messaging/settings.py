4f406f45f99bb6a0fc67aaf3bc7fab868b20b1c8
nilesh <youthtechh@gmail.com>
2018-05-28 12:11:34 +0530

    untruacked file issue fixed

--- messaging/settings.py
+++ messaging/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'messaging.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR + '/templates/',],
+        'DIRS': [BASE_DIR + '/templates1/',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
